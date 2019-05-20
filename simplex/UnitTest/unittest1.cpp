698e8457871b58826b8ba29eb8279e1e02488137
jaschn <49870201+jaschn@users.noreply.github.com>
2019-05-20 12:28:15 +0200

    console line argument

--- simplex/UnitTest/unittest1.cpp
+++ simplex/UnitTest/unittest1.cpp
@@ -4,6 +4,9 @@
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
+#define STRINGIFY(x) #x
+#define EXPAND(x) STRINGIFY(x)
+
 namespace UnitTest
 {		
 	TEST_CLASS(simplex_test)
@@ -11,8 +14,12 @@ namespace UnitTest
 	public:
 		TEST_METHOD(KI_5)
 		{
+			std::string s = EXPAND(UNITTESTPRJ);
+			s.erase(0, 1);
+			s.erase(s.size() - 2);
+			s += "..\\..\\KI_5.txt";
 			simplex a;
-			a.parse_file("C:/Users/Jakob/Documents/GitHub/simplex/KI_5.txt");
+			a.parse_file(s);
 			a.solve();
 			double a_res = a.get_results();
 			Assert::AreEqual(round(3.57142857142857), round(a_res));
@@ -20,8 +27,12 @@ namespace UnitTest
 
 		TEST_METHOD(KI_8)
 		{
+			std::string s = EXPAND(UNITTESTPRJ);
+			s.erase(0, 1);
+			s.erase(s.size() - 2);
+			s += "..\\..\\KI_8.txt";
 			simplex a;
-			a.parse_file("C:/Users/Jakob/Documents/GitHub/simplex/KI_8.txt");
+			a.parse_file(s);
 			a.solve();
 			double a_res = a.get_results();
 			Assert::AreEqual(round(14.5714285714286), round(a_res));
@@ -29,8 +40,12 @@ namespace UnitTest
 
 		TEST_METHOD(KI_9)
 		{
+			std::string s = EXPAND(UNITTESTPRJ);
+			s.erase(0, 1);
+			s.erase(s.size() - 2);
+			s += "..\\..\\KI_9.txt";
 			simplex a;
-			a.parse_file("C:/Users/Jakob/Documents/GitHub/simplex/KI_9.txt");
+			a.parse_file(s);
 			a.solve();
 			double a_res = a.get_results();
 			Assert::AreEqual(round(21.948275862069), round(a_res));
@@ -38,8 +53,12 @@ namespace UnitTest
 
 		TEST_METHOD(KI_10)
 		{
+			std::string s = EXPAND(UNITTESTPRJ);
+			s.erase(0, 1);
+			s.erase(s.size() - 2);
+			s += "..\\..\\KI_10.txt";
 			simplex a;
-			a.parse_file("C:/Users/Jakob/Documents/GitHub/simplex/KI_10.txt");
+			a.parse_file(s);
 			a.solve();
 			double a_res = a.get_results();
 			Assert::AreEqual(round(29.8095238095238), round(a_res));
@@ -47,8 +66,12 @@ namespace UnitTest
 
 		TEST_METHOD(KI_15)
 		{
+			std::string s = EXPAND(UNITTESTPRJ);
+			s.erase(0, 1);
+			s.erase(s.size() - 2);
+			s += "..\\..\\KI_15.txt";
 			simplex a;
-			a.parse_file("C:/Users/Jakob/Documents/GitHub/simplex/KI_15.txt");
+			a.parse_file(s);
 			a.solve();
 			double a_res = a.get_results();
 			Assert::AreEqual(round(36.6352941176471), round(a_res));
@@ -56,8 +79,12 @@ namespace UnitTest
 
 		TEST_METHOD(KI_20)
 		{
+			std::string s = EXPAND(UNITTESTPRJ);
+			s.erase(0, 1);
+			s.erase(s.size() - 2);
+			s += "..\\..\\KI_20.txt";
 			simplex a;
-			a.parse_file("C:/Users/Jakob/Documents/GitHub/simplex/KI_20.txt");
+			a.parse_file(s);
 			a.solve();
 			double a_res = a.get_results();
 			Assert::AreEqual(round(60.9032258064516), round(a_res));
@@ -65,8 +92,12 @@ namespace UnitTest
 
 		TEST_METHOD(KI_30)
 		{
+			std::string s = EXPAND(UNITTESTPRJ);
+			s.erase(0, 1);
+			s.erase(s.size() - 2);
+			s += "..\\..\\KI_30.txt";
 			simplex a;
-			a.parse_file("C:/Users/Jakob/Documents/GitHub/simplex/KI_30.txt");
+			a.parse_file(s);
 			a.solve();
 			double a_res = a.get_results();
 			Assert::AreEqual(round(261.768774703557), round(a_res));
@@ -76,8 +107,12 @@ namespace UnitTest
 
 		TEST_METHOD(KI_5_tolerance)
 		{
+			std::string s = EXPAND(UNITTESTPRJ);
+			s.erase(0, 1);
+			s.erase(s.size() - 2);
+			s += "..\\..\\KI_5.txt";
 			simplex a;
-			a.parse_file("C:/Users/Jakob/Documents/GitHub/simplex/KI_5.txt");
+			a.parse_file(s);
 			a.solve();
 			double a_res = a.get_results();
 			Assert::IsTrue(abs(3.57142857142857 - a_res) < tolerance);
@@ -85,8 +120,12 @@ namespace UnitTest
 
 		TEST_METHOD(KI_8_tolerance)
 		{
+			std::string s = EXPAND(UNITTESTPRJ);
+			s.erase(0, 1);
+			s.erase(s.size() - 2);
+			s += "..\\..\\KI_8.txt";
 			simplex a;
-			a.parse_file("C:/Users/Jakob/Documents/GitHub/simplex/KI_8.txt");
+			a.parse_file(s);
 			a.solve();
 			double a_res = a.get_results();
 			Assert::IsTrue(abs(14.5714285714286 - a_res) < tolerance);
@@ -94,8 +133,12 @@ namespace UnitTest
 
 		TEST_METHOD(KI_9_tolerance)
 		{
+			std::string s = EXPAND(UNITTESTPRJ);
+			s.erase(0, 1);
+			s.erase(s.size() - 2);
+			s += "..\\..\\KI_9.txt";
 			simplex a;
-			a.parse_file("C:/Users/Jakob/Documents/GitHub/simplex/KI_9.txt");
+			a.parse_file(s);
 			a.solve();
 			double a_res = a.get_results();
 			Assert::IsTrue(abs(21.948275862069 - a_res) < tolerance);
@@ -103,8 +146,12 @@ namespace UnitTest
 
 		TEST_METHOD(KI_10_tolerance)
 		{
+			std::string s = EXPAND(UNITTESTPRJ);
+			s.erase(0, 1);
+			s.erase(s.size() - 2);
+			s += "..\\..\\KI_10.txt";
 			simplex a;
-			a.parse_file("C:/Users/Jakob/Documents/GitHub/simplex/KI_10.txt");
+			a.parse_file(s);
 			a.solve();
 			double a_res = a.get_results();
 			Assert::IsTrue(abs(29.8095238095238 - a_res) < tolerance);
@@ -112,8 +159,12 @@ namespace UnitTest
 
 		TEST_METHOD(KI_15_tolerance)
 		{
+			std::string s = EXPAND(UNITTESTPRJ);
+			s.erase(0, 1);
+			s.erase(s.size() - 2);
+			s += "..\\..\\KI_15.txt";
 			simplex a;
-			a.parse_file("C:/Users/Jakob/Documents/GitHub/simplex/KI_15.txt");
+			a.parse_file(s);
 			a.solve();
 			double a_res = a.get_results();
 			Assert::IsTrue(abs(36.6352941176471 - a_res) < tolerance);
@@ -121,8 +172,12 @@ namespace UnitTest
 
 		TEST_METHOD(KI_20_tolerance)
 		{
+			std::string s = EXPAND(UNITTESTPRJ);
+			s.erase(0, 1);
+			s.erase(s.size() - 2);
+			s += "..\\..\\KI_20.txt";
 			simplex a;
-			a.parse_file("C:/Users/Jakob/Documents/GitHub/simplex/KI_20.txt");
+			a.parse_file(s);
 			a.solve();
 			double a_res = a.get_results();
 			Assert::IsTrue(abs(60.9032258064516 - a_res) < tolerance);
@@ -130,8 +185,12 @@ namespace UnitTest
 
 		TEST_METHOD(KI_30_tolerance)
 		{
+			std::string s = EXPAND(UNITTESTPRJ);
+			s.erase(0, 1);
+			s.erase(s.size() - 2);
+			s += "..\\..\\KI_30.txt";
 			simplex a;
-			a.parse_file("C:/Users/Jakob/Documents/GitHub/simplex/KI_30.txt");
+			a.parse_file(s);
 			a.solve();
 			double a_res = a.get_results();
 			Assert::IsTrue(abs(261.768774703557 - a_res) < tolerance);
--- simplex/simplex_console/simplex_console.cpp
+++ simplex/simplex_console/simplex_console.cpp
@@ -5,52 +5,12 @@
 #include <iostream>
 #include "../simplex/simplex.h"
 
-int main()
+int main(int argc, char *argv[])
 {
+	if (argc < 2)
+		return -1;
 	simplex a;
-	a.parse_file("../../KI_15.txt");
+	a.parse_file(argv[1]);
 	a.solve();
 	double a_res = a.get_results();
-
-	int x = 0;
-	//simplex b;
-	//b.parse_file("../../KI_8.txt");
-	//b.solve();
-	//double b_res = b.get_results();
-
-	//simplex c;
-	//c.parse_file("../../KI_9.txt");
-	//c.solve();
-	//double c_res = c.get_results();
-
-	//simplex d;
-	//d.parse_file("../../KI_10.txt");
-	//d.solve();
-	//double d_res = d.get_results();
-
-	//simplex e;
-	//e.parse_file("../../KI_15.txt");
-	//e.solve();
-	//double e_res = e.get_results();
-
-	//simplex f;
-	//f.parse_file("../../KI_20.txt");
-	//f.solve();
-	//double f_res = f.get_results();
-
-	//simplex g;
-	//g.parse_file("../../KI_20.txt");
-	//g.solve();
-	//double g_res = g.get_results();
 }
-
-// Programm ausführen: STRG+F5 oder "Debuggen" > Menü "Ohne Debuggen starten"
-// Programm debuggen: F5 oder "Debuggen" > Menü "Debuggen starten"
-
-// Tipps für den Einstieg: 
-//   1. Verwenden Sie das Projektmappen-Explorer-Fenster zum Hinzufügen/Verwalten von Dateien.
-//   2. Verwenden Sie das Team Explorer-Fenster zum Herstellen einer Verbindung mit der Quellcodeverwaltung.
-//   3. Verwenden Sie das Ausgabefenster, um die Buildausgabe und andere Nachrichten anzuzeigen.
-//   4. Verwenden Sie das Fenster "Fehlerliste", um Fehler anzuzeigen.
-//   5. Wechseln Sie zu "Projekt" > "Neues Element hinzufügen", um neue Codedateien zu erstellen, bzw. zu "Projekt" > "Vorhandenes Element hinzufügen", um dem Projekt vorhandene Codedateien hinzuzufügen.
-//   6. Um dieses Projekt später erneut zu öffnen, wechseln Sie zu "Datei" > "Öffnen" > "Projekt", und wählen Sie die SLN-Datei aus.
